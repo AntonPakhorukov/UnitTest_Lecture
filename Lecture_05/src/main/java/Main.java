98c8f328595d429ab68bed33e7b24e9358a0cc27
ÄntonPakhorukov <pakhorukov.a@mail.ru>
2023-10-13 00:15:34 +0700

    not work 2

--- Lecture_05/src/main/java/Main.java
+++ Lecture_05/src/main/java/Main.java
@@ -4,13 +4,22 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 public class Main {
     public static void main(String[] args) throws InterruptedException{
+        // подключаем chrome driver, говорим системе, где находится инструмент,
+        // с помощью которого будем взаимодействовать с браузером
         System.setProperty("webdriver.chrome.driver", "D:\\Geek Brains 2022\\UnitTest\\Selenium\\chromedriver_win32\\chromedriver.exe");
+        // создаем экземпляр chrome драйвера
         WebDriver driver = new ChromeDriver();
+        // запрашиваем главную страницу гугла
         driver.get("https://google.com");
+        // ищем поле для ввода поискового запроса
         WebElement searchBox = driver.findElement(By.name("q"));
+        // вводим в поле GeekBrains
         searchBox.sendKeys("GeekBrains");
+        // отправляем форму
         searchBox.submit();
+        // ждем 10 секунд
         Thread.sleep(10000);
+        // закрываем окно браузера
         driver.close();
     }
 }
